d13331e9942fb9a64afd67ab73cac0baf0af8c1a
sivasaikumari <59605547+sivasaikumari@users.noreply.github.com>
2020-01-14 10:28:55 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,2 +1,9 @@
 # 7978_01_python.py
-df=pd.read_csv(" ");
+import pandas as pd
+import numpy as np
+df=pd.read_csv("C:\\Users\\siva.maineni\\Downloads\\movie_metadata.csv"
+temp=df.groupby('title_year')['genres'].unique()
+genre_combo=pd.DataFrame(temp)
+genre_combo['avg_rating']=df.groupby(['genres'])['imdb_score'].mean()
+genre_combo['max_rating']=df.groupby(['genres'])['imdb_score'].max()
+genre_combo['min_rating']=df.groupby(['genres'])['imdb_score'].min()

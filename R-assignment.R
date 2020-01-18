d884efa03025ed8dc408e8bbb8b6dcf6eef667d6
sivasaikumari <59605547+sivasaikumari@users.noreply.github.com>
2020-01-18 12:54:45 +0530

    Update R-assignment.R

--- R-assignment.R
+++ R-assignment.R
@@ -89,7 +89,73 @@ impute <-function(df)
   return(df)
 }
 
+#2Ans
+dataframe <- read.csv('C:/Users/siva.maineni/Downloads/diamond.csv')
+q2<-function(dataframe)
+{
+  dataframe1$length1<-nchar(gsub(" ","",dataframe1$wordsInTitle,fixed=TRUE))
+  dataframe1$quantile<-as.numeric(ntile(dataframe1$length1,4))
+  df3<-as.data.frame.matrix(table(dataframe1$year,dataframe1$quantile))
+  df4<-dataframe1%>%group_by(year)%>%summarise(minimum=min(length1,na.rm=TRUE),maximum=max(length1,na.rm=TRUE))
+  df4=drop_na(df4)
+  df5<-cbind(df4,df3)
+  row.names(df5)<-NULL
+  return(df5)
+}
+#q2(dataframe)
 
+
+#3Ans
+dataframe <- read.csv('C:/Users/siva.maineni/Downloads/diamonds.csv')
+q3<-function(dataframe)
+{
+  depth.filter<-dataframe$depth>60
+  v<-c()
+  for(i in 1:length(depth.filter))
+    {
+      if(depth.filter[i])
+        {
+          vol<-(as.numeric(as.character(dataframe$x[i]))*
+          as.numeric(as.character(dataframe$y[i]))*
+          as.numeric(as.character(dataframe$z[i])))
+        }
+      else
+      {
+        vol<-8
+      }
+    v<-c(v,vol)
+  }
+  dataframe$volume<-v
+  dataframe$Bins<-as.numeric(ntile(dataframe$volume,5))
+  tvol=sum(dataframe$volume)
+  t<-crosstab(dataframe$Bins,dataframe$cut,plot=FALSE,prop.t=TRUE)
+  return(t)
+}
+#q3(dataframe)
+
+
+#5Ans
+df2<-read_delim("c"/Users/siva.maineni/Downloads/imdb.csv", delim=',', escape_double=FALSE, escape_backslash=TRUE)
+q5<-function(df)
+{
+  df2<-na_mean(df2)
+  df2$deciles<-as.numeric(ntile(df2$duration,10))
+  df3<-df2%>%group_by(deciles)%>%summarise(nr_Of_Nominations=sum(nrOfNominations,na.rm=TRUE),nr_Of_Wins=sum(nrOfWins))
+  df3$count<-as.data.frame(table(df2$deciles))['Freq']
+  df4<-df2[c(17:45)]
+  df5<-df4%>%group_by(deciles)%>%summarise_all(sum)
+  df5<-as.data.frame.matrix(t(df5))
+  names<-row.names(df5)
+  high<-function(x)
+  {
+    return(names[order(x,decreasing = TRUE)[1:3]])
+  }
+  df6<-as.data.frame.matrix(t(sapply(df5,high)))
+  colnames(df6)<-c('first','second','third')
+  df3['top genres']<-paste(df6$first,',',df6$second,',',df6$third)
+  return(df3)
+}
+#q5(df2)
 
 
 
